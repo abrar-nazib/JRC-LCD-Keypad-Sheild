c8601420a95fecbfefc18ab0042673fb9e5470e3
abrar-nazib <abrarnazib@gmail.com>
2023-07-22 16:52:56 +0600

    Testing done of the Sheild Library

---
+++ include/JRC.h
@@ -0,0 +1,34 @@
+#ifndef _JRC
+#define _JRC
+
+// Mapping Arduino Digital Pins to JRC GPIO
+#define ARD_D0 3
+#define ARD_D1 1
+#define ARD_D2 0
+#define ARD_D3 2
+#define ARD_D4 4
+#define ARD_D5 17
+#define ARD_D6 16
+#define ARD_D7 15
+#define ARD_D8 14
+#define ARD_D9 13
+#define ARD_D10 5
+#define ARD_D11 23
+#define ARD_D12 19
+#define ARD_D13 18
+#define ARD_D14 26
+#define ARD_D15 25
+#define ARD_D16 35
+#define ARD_D17 34
+#define ARD_D18 33 // I2C SDA in 21
+#define ARD_D19 32 // I2C SCL in 22
+
+// Mapping Arduino Analog Pins to JRC GPIO
+#define ARD_A0 26
+#define ARD_A1 25
+#define ARD_A2 35
+#define ARD_A3 34
+#define ARD_A4 33
+#define ARD_A5 32
+
+#endif // _JRC
